8c17bce34195f4559231b928f7d373b68e32f092
Kevin Specht <kspecht@mitre.org>
2024-04-30 16:00:56 -0400

    function return

--- Function/Function.cpp
+++ Function/Function.cpp
@@ -14,12 +14,25 @@ void doA()
     std::cout << "Ending doA()\n";
 }
 
+int getValueFromUser() // this function now returns an integer value
+{
+	std::cout << "Enter an integer: ";
+	int input{};
+	std::cin >> input;
+
+	return input; // return the value the user entered back to the caller
+}
+
 // Definition of function main()
 int main()
 {
     std::cout << "Starting main()\n";
 
     doA();
+
+    int num { getValueFromUser() }; // initialize num with the return value of getValueFromUser()
+
+    std::cout << num << " doubled is: " << num * 2 << '\n';
 
     std::cout << "Ending main()\n";
 
